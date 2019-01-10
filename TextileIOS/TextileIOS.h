49104d785db0109fb4794d3e0deee865207e60ee
Aaron Sutula <aaron.sutula@gmail.com>
2019-01-09 19:02:20 -0600

    Adding Xcode files

---
+++ TextileIOS/TextileIOS.h
@@ -0,0 +1,13 @@
+//
+//  TextileIOS.h
+//  TextileIOS
+//
+//  Created by Aaron Sutula on 1/9/19.
+//  Copyright © 2019 Textile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TextileIOS : NSObject
+
+@end
---
+++ TextileIOS/TextileIOS.m
@@ -0,0 +1,13 @@
+//
+//  TextileIOS.m
+//  TextileIOS
+//
+//  Created by Aaron Sutula on 1/9/19.
+//  Copyright © 2019 Textile. All rights reserved.
+//
+
+#import "TextileIOS.h"
+
+@implementation TextileIOS
+
+@end
